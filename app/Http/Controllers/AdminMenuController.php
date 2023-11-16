d911ca0fcd8dc3c1cf6e0fa3cde96f5a8c764a08
mhaitsam18 <haitsam03@gmail.com>
2023-11-16 17:59:39 +0700

    CRUD Jadwal Menu beres

--- app/Http/Controllers/AdminMenuController.php
+++ app/Http/Controllers/AdminMenuController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Menu;
 use App\Models\Paket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminMenuController extends Controller
 {
@@ -26,7 +27,11 @@ class AdminMenuController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.menu.create', [
+            'title' => 'Umi Tika Catering | Tambah Menu',
+            'page' => 'menu',
+            'pakets' => Paket::all()
+        ]);
     }
 
     /**
@@ -34,7 +39,25 @@ class AdminMenuController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'menu' => 'required|string',
+            'waktu_makan' => 'required|in:breakfast,lunch,dinner',
+            'tanggal' => 'required|date',
+            'paket_id' => 'required|exists:paket,id',
+            'gambar' => 'required|image|mimes:jpeg,png,jpg,gif|max:20480', // maksimum 20MB
+        ]);
+
+        $gambarPath = $request->file('gambar')->store('menu');
+
+        Menu::create([
+            'menu' => $request->menu,
+            'waktu_makan' => $request->waktu_makan,
+            'tanggal' => $request->tanggal,
+            'paket_id' => $request->paket_id,
+            'gambar' => $gambarPath,
+        ]);
+
+        return back()->with('success', 'Menu berhasil ditambahkan');
     }
 
     /**
@@ -42,7 +65,11 @@ class AdminMenuController extends Controller
      */
     public function show(Menu $menu)
     {
-        //
+        return view('admin.menu.show', [
+            'title' => 'Umi Tika Catering | Detail Menu',
+            'page' => 'menu',
+            'menu' => $menu
+        ]);
     }
 
     /**
@@ -50,7 +77,12 @@ class AdminMenuController extends Controller
      */
     public function edit(Menu $menu)
     {
-        //
+        return view('admin.menu.edit', [
+            'title' => 'Umi Tika Catering | Edit Menu',
+            'page' => 'menu',
+            'menu' => $menu,
+            'pakets' => Paket::all()
+        ]);
     }
 
     /**
@@ -58,7 +90,32 @@ class AdminMenuController extends Controller
      */
     public function update(Request $request, Menu $menu)
     {
-        //
+        $request->validate([
+            'menu' => 'required|string',
+            'waktu_makan' => 'required|in:breakfast,lunch,dinner',
+            'tanggal' => 'required|date',
+            'paket_id' => 'required|exists:paket,id',
+            'gambar' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:20480', // maksimum 20MB
+        ]);
+
+        if ($request->hasFile('gambar')) {
+            // Hapus gambar lama jika ada
+            Storage::delete($menu->gambar);
+
+            $gambarPath = $request->file('gambar')->store('menu');
+        } else {
+            $gambarPath = $menu->gambar;
+        }
+
+        $menu->update([
+            'menu' => $request->menu,
+            'waktu_makan' => $request->waktu_makan,
+            'tanggal' => $request->tanggal,
+            'paket_id' => $request->paket_id,
+            'gambar' => $gambarPath,
+        ]);
+
+        return back()->with('success', 'Menu berhasil diperbarui');
     }
 
     /**
@@ -66,6 +123,11 @@ class AdminMenuController extends Controller
      */
     public function destroy(Menu $menu)
     {
-        //
+        // Hapus gambar sebelum menghapus data menu
+        Storage::delete($menu->gambar);
+
+        $menu->delete();
+
+        return back()->with('success', 'Menu berhasil dihapus');
     }
 }
